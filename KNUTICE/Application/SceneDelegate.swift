e300784180ff786acafd53a18fc3daf91c04e77b
jeungHunLee <ksjs1111@gmail.com>
2024-07-08 14:48:30 +0900

    FEAT: 다크모드 비활성화

--- KNUTICE/Application/SceneDelegate.swift
+++ KNUTICE/Application/SceneDelegate.swift
@@ -18,6 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScen = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScen)
+        self.window?.overrideUserInterfaceStyle = .light    //Dark Mode disable
         
         let mainViewController = MainViewController()
         mainViewController.bind()
