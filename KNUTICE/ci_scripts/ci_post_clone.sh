d45156020778f3a9933396b0e008f4738b986a36
jeungHunLee <ksjs1111@gmail.com>
2024-08-08 15:01:00 +0900

    CI: ci script 파일 수정

--- KNUTICE/ci_scripts/ci_post_clone.sh
+++ KNUTICE/ci_scripts/ci_post_clone.sh
@@ -18,7 +18,7 @@ convert_bool() {
     fi
 }
 
-cat <<EOF > "/Downloads/GoogleService-Info.plist"
+cat <<EOF > "/Volumes/workspace/repository/KNUTICE/GoogleService-Info.plist"
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
