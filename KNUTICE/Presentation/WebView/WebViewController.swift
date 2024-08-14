aff2da2ab45199dd33ee8f477dbb74aed6b5e95e
jeungHunLee <ksjs1111@gmail.com>
2024-08-15 08:58:12 +0900

    FEAT: window.open() 호출 시 기본 웹 브라우저로 이동

--- KNUTICE/Presentation/WebView/WebViewController.swift
+++ KNUTICE/Presentation/WebView/WebViewController.swift
@@ -52,15 +52,9 @@ extension WebViewController: WKNavigationDelegate, WKUIDelegate {
     }
     
     func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
-//        if let aString = URL(string:(navigationAction.request.url?.absoluteString ?? "")) {
-//            UIApplication.shared.open(aString, options:[:]) { _ in }
-//        }
-        
-        guard let urlString = navigationAction.request.url?.absoluteString else {
-            return nil
+        if let aString = URL(string:(navigationAction.request.url?.absoluteString ?? "")) {
+            UIApplication.shared.open(aString, options:[:]) { _ in }
         }
-        
-        self.present(DownloadWebViewController(url: urlString), animated: true)
         
         return nil
     }
