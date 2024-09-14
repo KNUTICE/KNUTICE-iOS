314094caa835a2785cc3586bf851325131d1cff1
jeungHunLee <ksjs1111@gmail.com>
2024-09-14 14:32:27 +0900

    FEAT: 메인 View 새로고침 기능 추가

--- KNUTICE/Presentation/Main/View/MainViewController+Binding.swift
+++ KNUTICE/Presentation/Main/View/MainViewController+Binding.swift
@@ -29,4 +29,18 @@ extension MainViewController {
             .bind(to: tableView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
     }
+    
+    func bindRefreshControl() {
+        //ViewModel의 isLoading의 값이 변경 되었을 때 refreshControl의 isRegreshing의 값 변경되도록 바인딩
+        viewModel.isLoading
+            .bind(to: refreshControl.rx.isRefreshing)
+            .disposed(by: disposeBag)
+        
+        //RefreshControl의 valueChanged 이벤트 관찰 후 수행할 작업
+        refreshControl.rx.controlEvent(.valueChanged)
+            .bind(onNext: { [weak self] in
+                self?.viewModel.refreshNotices()
+            })
+            .disposed(by: disposeBag)
+    }
 }
