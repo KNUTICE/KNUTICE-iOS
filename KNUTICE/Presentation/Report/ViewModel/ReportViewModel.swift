8d95d64a9af59906ad390e6f965a8445d4ad82f1
jeungHunLee <ksjs1111@gmail.com>
2024-09-22 17:14:23 +0900

    FEAT: 리포트 api 연동

--- KNUTICE/Presentation/Report/ViewModel/ReportViewModel.swift
+++ KNUTICE/Presentation/Report/ViewModel/ReportViewModel.swift
@@ -6,7 +6,57 @@
 //
 
 import Combine
+import Foundation
 
 final class ReportViewModel: ObservableObject {
+    enum AlertType {
+        case success
+        case failure
+        case textOverFlow
+        case none
+    }
+    
     @Published var content: String = ""
+    @Published var isShowingAlert: Bool = false
+    @Published var isLoading: Bool = false
+    
+    var alertType: AlertType = .none
+    private let reportService: ReportService
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(reportService: ReportService) {
+        self.reportService = reportService
+    }
+    
+    func report(device: String) {
+        guard 500 >= content.count else {
+            alertType = .textOverFlow
+            isShowingAlert.toggle()
+            return
+        }
+        
+        isLoading.toggle()
+        reportService.report(content: content, device: device)
+            .sink(receiveCompletion: { [weak self] completion in
+                self?.isLoading.toggle()
+                
+                switch completion {
+                case .finished:
+                    print("The request to report has been completed")
+                case .failure(let error):
+                    print("ReportViewModel.report: \(error)")
+                    self?.alertType = .failure
+                    self?.isShowingAlert.toggle()
+                }
+            }, receiveValue: { [weak self] in
+                if $0 {
+                    self?.alertType = .success
+                } else {
+                    self?.alertType = .failure
+                }
+                
+                self?.isShowingAlert.toggle()
+            })
+            .store(in: &cancellables)
+    }
 }
