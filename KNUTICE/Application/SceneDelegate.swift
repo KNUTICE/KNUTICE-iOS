77e2f87fed53ddd886071f32fd781f1246aaefe0
jeungHunLee <ksjs1111@gmail.com>
2024-09-22 17:24:06 +0900

    REFACTOR: ViewModel 의존성 주입 리팩토링

--- KNUTICE/Application/SceneDelegate.swift
+++ KNUTICE/Application/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScen = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScen)
         
-        let mainViewController = MainViewController(viewModel: AppDI.shared.mainViewModel)
+        let mainViewController = MainViewController(viewModel: AppDI.shared.makeMainViewModel())
         mainViewController.bind()
         mainViewController.bindRefreshControl()
         let navigationViewController = UINavigationController(rootViewController: mainViewController)
