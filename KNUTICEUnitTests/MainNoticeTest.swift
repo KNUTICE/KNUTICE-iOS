d48dbc8404430f086579f1a02aef49bbca9ea0ab
jeungHunLee <ksjs1111@gmail.com>
2024-09-24 15:38:56 +0900

    TEST: 테스트 코드 추가

--- KNUTICEUnitTests/MainNoticeTest.swift
+++ KNUTICEUnitTests/MainNoticeTest.swift
@@ -6,30 +6,36 @@
 //
 
 import XCTest
+@testable import KNUTICE
 
 final class MainNoticeTest: XCTestCase {
+    private var mainNoticeDatasource: MainNoticeDataSource!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        mainNoticeDatasource = MainNoticeDataSourceImpl()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_fetch_mainNotice() throws {
+        //Given
+        let expectation = XCTestExpectation()
+        
+        //When - 메인 공지사항 요청 테스트
+        mainNoticeDatasource.fetchMainNotices()
+            .subscribe(onNext: {
+                //Then
+                let result = try? $0.get()
+                
+                XCTAssertEqual(result?.result.resultCode, 200)
+                expectation.fulfill()
+            })
+        
+        wait(for: [expectation], timeout: 10)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
+
