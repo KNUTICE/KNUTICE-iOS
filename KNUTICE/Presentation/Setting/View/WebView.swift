822db1a488d14db327a6fd7c3fe58e7159da60ac
jeungHunLee <ksjs1111@gmail.com>
2024-10-01 12:54:18 +0900

    FIX: 다크 모드에서 웹뷰 tint color가 보이지 않는 문제 수정

--- KNUTICE/Presentation/Setting/View/WebView.swift
+++ KNUTICE/Presentation/Setting/View/WebView.swift
@@ -24,6 +24,7 @@ struct WebView: UIViewRepresentable {
             return webView
         }
         
+        webView.tintColor = .black
         webView.navigationDelegate = context.coordinator
         webView.load(URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad))
         
