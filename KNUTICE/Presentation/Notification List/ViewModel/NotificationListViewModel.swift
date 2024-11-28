cad45b92aada681d9a4c5bd5aea6f76dd2714a4b
jeungHunLee <ksjs1111@gmail.com>
2024-11-28 09:26:08 +0900

    FEAT: 카테고리 별 알림 토글 UI 추가

--- KNUTICE/Presentation/Notification List/ViewModel/NotificationListViewModel.swift
+++ KNUTICE/Presentation/Notification List/ViewModel/NotificationListViewModel.swift
@@ -9,25 +9,48 @@ import Combine
 import os
 
 final class NotificationListViewModel: ObservableObject {
+    @Published var isGeneralNoticeNotificationAllowed: Bool?
+    @Published var isAcademicNoticeNotificationAllowd: Bool?
+    @Published var isScholarshipNoticeNotificationAllowed: Bool?
+    @Published var isEventNoticeNotificationAllowed: Bool?
+    @Published var isLoading: Bool = false
+    
     private let repository: NotificationRepository
     private let logger = Logger()
     private var cancellables = Set<AnyCancellable>()
+    
+    var isAllDataLoaded: Bool {
+        let data = [
+            isGeneralNoticeNotificationAllowed,
+            isAcademicNoticeNotificationAllowd,
+            isScholarshipNoticeNotificationAllowed,
+            isEventNoticeNotificationAllowed
+        ]
+        
+        return data.allSatisfy { $0 == true }
+    }
     
     init(repository: NotificationRepository) {
         self.repository = repository
     }
     
     func getNotificationPermissions() {
+        isLoading = true
         repository.getNotificationPermissions()
+            .receive(on: DispatchQueue.main)
             .sink(receiveCompletion: { [weak self] completion in
                 switch completion {
                 case .finished:
+                    self?.isLoading = false
                     self?.logger.log(level: .debug, "Notification permission request finished.")
                 case .failure(let error):
                     self?.logger.log(level: .error, "NotificationListViewModel: \(error.localizedDescription)")
                 }
-            }, receiveValue: {
-                print($0)
+            }, receiveValue: { [weak self] in
+                self?.isGeneralNoticeNotificationAllowed = $0["generalNotice"]
+                self?.isAcademicNoticeNotificationAllowd = $0["academicNotice"]
+                self?.isScholarshipNoticeNotificationAllowed = $0["scholarshipNotice"]
+                self?.isEventNoticeNotificationAllowed = $0["eventNotice"]
             })
             .store(in: &cancellables)
     }
