588405e5a9718e3685777b2e1a83294932615e28
jeungHunLee <ksjs1111@gmail.com>
2024-11-28 09:26:08 +0900

    FEAT: 카테고리 별 알림 토글 UI 추가

--- KNUTICE/Presentation/Notification List/View/NotificationList.swift
+++ KNUTICE/Presentation/Notification List/View/NotificationList.swift
@@ -15,13 +15,61 @@ struct NotificationList: View {
     }
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .onAppear {
+        List {
+            Section {
+                Toggle(isOn: Binding(get: {
+                    viewModel.isGeneralNoticeNotificationAllowed ?? false
+                }, set: { _ in
+                    
+                }), label: {
+                    Text("일반공지")
+                })
+                
+                Toggle(isOn: Binding(get: {
+                    viewModel.isAcademicNoticeNotificationAllowd ?? false
+                }, set: { _ in
+                    
+                }), label: {
+                    Text("학사공지")
+                })
+                
+                Toggle(isOn: Binding(get: {
+                    viewModel.isScholarshipNoticeNotificationAllowed ?? false
+                }, set: { _ in
+                    
+                }), label: {
+                    Text("장학안내")
+                })
+                
+                Toggle(isOn: Binding(get: {
+                    viewModel.isEventNoticeNotificationAllowed ?? false
+                }, set: { _ in
+                    
+                }), label: {
+                    Text("행사안내")
+                })
+            } footer: {
+                Text("새로운 공지사항이 등록되면 푸시 알림으로 알려드려요.")
+                    .font(.caption2)
+                    .foregroundStyle(.gray)
+            }
+            .padding([.top, .bottom])
+            .listSectionSeparator(.hidden, edges: .bottom)
+        }
+        .tint(.toggle)
+        .listStyle(.plain)
+        .navigationTitle("서비스 알림")
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            if !viewModel.isAllDataLoaded {
                 viewModel.getNotificationPermissions()
             }
+        }
     }
 }
 
 #Preview {
-    NotificationList(viewModel: AppDI.shared.makeNotificationListViewModel())
+    NavigationStack {
+        NotificationList(viewModel: AppDI.shared.makeNotificationListViewModel())
+    }
 }
