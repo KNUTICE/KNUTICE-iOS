9ab77b0cb9f33f495e6d14a31be78f1fde6a7446
jeungHunLee <ksjs1111@gmail.com>
2024-12-22 13:46:38 +0900

    FEAT: 주제 구독 네트워크 요청 중 로딩 Indicator 추가

--- KNUTICE/Presentation/Notification List/View/NotificationList.swift
+++ KNUTICE/Presentation/Notification List/View/NotificationList.swift
@@ -15,54 +15,60 @@ struct NotificationList: View {
     }
     
     var body: some View {
-        List {
-            Section {
-                Toggle(isOn: Binding(get: {
-                    viewModel.isGeneralNoticeNotificationAllowed ?? false
-                }, set: {
-                    viewModel.update(key: .generalNotice, value: $0)
-                }), label: {
-                    Text("일반공지")
-                })
-                
-                Toggle(isOn: Binding(get: {
-                    viewModel.isAcademicNoticeNotificationAllowd ?? false
-                }, set: {
-                    viewModel.update(key: .academicNotice, value: $0)
-                }), label: {
-                    Text("학사공지")
-                })
-                
-                Toggle(isOn: Binding(get: {
-                    viewModel.isScholarshipNoticeNotificationAllowed ?? false
-                }, set: {
-                    viewModel.update(key: .scholarshipNotice, value: $0)
-                }), label: {
-                    Text("장학안내")
-                })
-                
-                Toggle(isOn: Binding(get: {
-                    viewModel.isEventNoticeNotificationAllowed ?? false
-                }, set: {
-                    viewModel.update(key: .eventNotice, value: $0)
-                }), label: {
-                    Text("행사안내")
-                })
-            } footer: {
-                Text("새로운 공지사항이 등록되면 푸시 알림으로 알려드려요.")
-                    .font(.caption2)
-                    .foregroundStyle(.gray)
+        ZStack {
+            List {
+                Section {
+                    Toggle(isOn: Binding(get: {
+                        viewModel.isGeneralNoticeNotificationAllowed ?? false
+                    }, set: {
+                        viewModel.update(key: .generalNotice, value: $0)
+                    }), label: {
+                        Text("일반공지")
+                    })
+                    
+                    Toggle(isOn: Binding(get: {
+                        viewModel.isAcademicNoticeNotificationAllowd ?? false
+                    }, set: {
+                        viewModel.update(key: .academicNotice, value: $0)
+                    }), label: {
+                        Text("학사공지")
+                    })
+                    
+                    Toggle(isOn: Binding(get: {
+                        viewModel.isScholarshipNoticeNotificationAllowed ?? false
+                    }, set: {
+                        viewModel.update(key: .scholarshipNotice, value: $0)
+                    }), label: {
+                        Text("장학안내")
+                    })
+                    
+                    Toggle(isOn: Binding(get: {
+                        viewModel.isEventNoticeNotificationAllowed ?? false
+                    }, set: {
+                        viewModel.update(key: .eventNotice, value: $0)
+                    }), label: {
+                        Text("행사안내")
+                    })
+                } footer: {
+                    Text("새로운 공지사항이 등록되면 푸시 알림으로 알려드려요.")
+                        .font(.caption2)
+                        .foregroundStyle(.gray)
+                }
+                .padding([.top, .bottom])
+                .listSectionSeparator(.hidden, edges: .bottom)
             }
-            .padding([.top, .bottom])
-            .listSectionSeparator(.hidden, edges: .bottom)
-        }
-        .tint(.toggle)
-        .listStyle(.plain)
-        .navigationTitle("서비스 알림")
-        .navigationBarTitleDisplayMode(.inline)
-        .onAppear {
-            if !viewModel.isAllDataLoaded {
-                viewModel.getNotificationPermissions()
+            .tint(.toggle)
+            .listStyle(.plain)
+            .navigationTitle("서비스 알림")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                if !viewModel.isAllDataLoaded {
+                    viewModel.getNotificationPermissions()
+                }
+            }
+            
+            if viewModel.isLoading {
+                SpinningIndicator()
             }
         }
     }
