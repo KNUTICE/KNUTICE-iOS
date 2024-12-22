1293e90e0b5a7714927447441171f4f30df1beab
jeungHunLee <ksjs1111@gmail.com>
2024-12-22 14:30:46 +0900

    TEST: 검색 Unit Test 추가

---
+++ KNUTICEUnitTests/SearchTest.swift
@@ -0,0 +1,40 @@
+//
+//  SearchTest.swift
+//  KNUTICEUnitTests
+//
+//  Created by 이정훈 on 12/22/24.
+//
+
+import XCTest
+@testable import KNUTICE
+
+final class SearchTest: XCTestCase {
+    private var dataSource: NoticeDataSource!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        dataSource = NoticeDataSourceImpl()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
+        dataSource = nil
+    }
+    
+    func test_network_request() throws {
+        //Given
+        let expectation = XCTestExpectation(description: "netword request test")
+        let url = Bundle.main.searchURL + "?keyword=공지"
+        
+        //When
+        let _ = dataSource.fetchNotices(from: url)
+            .subscribe(onSuccess: { dto in
+                XCTAssertEqual(dto.result.resultCode, 200)
+                expectation.fulfill()
+            })
+        
+        wait(for: [expectation], timeout: 10)
+    }
+}
