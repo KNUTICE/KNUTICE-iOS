5916afcd8aa0e16ffbdd0032266f1d508d63afe1
jeungHunLee <ksjs1111@gmail.com>
2025-01-20 09:13:29 +0900

    FEAT: Bookmark 알림 시 앱 아이콘 배지 추가

--- KNUTICE/Services/BookmarkService.swift
+++ KNUTICE/Services/BookmarkService.swift
@@ -5,4 +5,25 @@
 //  Created by 이정훈 on 1/18/25.
 //
 
-import Foundation
+import Combine
+import UserNotifications
+
+protocol BookmarkService {
+    func save(bookmark: Bookmark) -> AnyPublisher<Void, any Error>
+}
+
+final class BookmarkServiceImpl: BookmarkService {
+    private let repository: BookmarkRepository
+    
+    init(repository: BookmarkRepository) {
+        self.repository = repository
+    }
+    
+    func save(bookmark: Bookmark) -> AnyPublisher<Void, any Error> {
+        return UNUserNotificationCenter.current().registerLocalNotification(for: bookmark)
+            .flatMap { _ -> AnyPublisher<Void, any Error> in
+                self.repository.save(bookmark: bookmark)
+            }
+            .eraseToAnyPublisher()
+    }
+}
