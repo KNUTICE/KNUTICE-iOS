243cea636007b628dfeed9d40870b2f0b4f7b746
jeungHunLee <ksjs1111@gmail.com>
2025-01-24 13:43:19 +0900

    TEST: Main Popup 테스트 케이스 추가

---
+++ KNUTICEUnitTests/MainPopupTest.swift
@@ -0,0 +1,45 @@
+//
+//  MainPopupTest.swift
+//  KNUTICEUnitTests
+//
+//  Created by 이정훈 on 1/24/25.
+//
+
+import XCTest
+import Combine
+@testable import KNUTICE
+
+final class MainPopupTest: XCTestCase {
+    private var dataSource: RemoteDataSource!
+    private var cancellables: Set<AnyCancellable>!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        dataSource = RemoteDataSourceImpl.shared
+        cancellables = []
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test_fetch_returnsValidResponse() {
+        //Given
+        let expectation = XCTestExpectation(description: "network request test")
+        let apiEndPoint = Bundle.main.mainPopupContentURL
+        
+        //When
+        dataSource.sendGetRequest(to: apiEndPoint, resultType: MainPopupContentDTO.self)
+            .sink(receiveCompletion: {
+                print($0)
+            }, receiveValue: {
+                //Then
+                XCTAssertEqual($0.result.resultCode, 200)
+                expectation.fulfill()
+            })
+            .store(in: &cancellables)
+        
+        wait(for: [expectation], timeout: 10)
+    }
+}
