ab89245400b9f82a2136f667a4f17ab0f5dbf90a
jeungHunLee <ksjs1111@gmail.com>
2025-02-19 09:02:47 +0900

    RENAME: Test -> Tests 이름 변경

--- KNUTICEUnitTests/MainPopupTests.swift
+++ KNUTICEUnitTests/MainPopupTests.swift
@@ -10,7 +10,7 @@ import Combine
 import Factory
 @testable import KNUTICE
 
-final class MainPopupTest: XCTestCase {
+final class MainPopupTests: XCTestCase {
     private var dataSource: RemoteDataSource!
     private var cancellables: Set<AnyCancellable>!
 
