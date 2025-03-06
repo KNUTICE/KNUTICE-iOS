5104a1364a0c430ab7793c43be5501b9f8b791bd
jeungHunLee <ksjs1111@gmail.com>
2025-03-06 10:14:07 +0900

    FIX: Xcode Cloud 시뮬레이터 환경에서 apns token을 사용할 수 없는 문제 수정

--- KNUTICE/Repositories/TokenRepositoryImpl.swift
+++ KNUTICE/Repositories/TokenRepositoryImpl.swift
@@ -36,11 +36,17 @@ final class TokenRepositoryImpl: TokenRepository {
     
     func getFCMToken() -> AnyPublisher<String, any Error> {
         return Future { promise in
-            Messaging.messaging().token { token, error in
-                if let error = error {
-                    promise(.failure(error))
-                } else {
-                    promise(.success(token ?? ""))
+            if ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil {
+                //Firebase 10.4.0 SDK를 사용하는 UnitTest에서 iOS 16 Simulator와 Xcode 13,  Apple Silicon HW를 만족하지 않으면 토큰을 사용할 수 없는 이슈
+                //UnitTest 환경에서 임의의 토큰 정보 반환
+                promise(.success(""))
+            } else {
+                Messaging.messaging().token { token, error in
+                    if let error = error {
+                        promise(.failure(error))
+                    } else {
+                        promise(.success(token ?? ""))
+                    }
                 }
             }
         }
