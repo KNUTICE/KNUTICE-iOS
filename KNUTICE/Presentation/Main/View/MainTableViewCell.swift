6b1e12c73d0af1b1a85c1f2b73f3c9823caad0ef
jeungHunLee <ksjs1111@gmail.com>
2025-03-22 22:22:50 +0900

    FIX: MainTableViewCell 레이아웃이 깨지는 버그 수정

--- KNUTICE/Presentation/Main/View/MainTableViewCell.swift
+++ KNUTICE/Presentation/Main/View/MainTableViewCell.swift
@@ -8,23 +8,25 @@
 import UIKit
 import SnapKit
 
-final class MainTableViewCell: UITableViewCell {
-    static let reuseIdentifier = "MainListCell"
-    private let titleLabel: UILabel = {
+class MainTableViewCell: UITableViewCell {
+    class var reuseIdentifier: String {
+        "MainTableViewCell"
+    }
+    let titleLabel: UILabel = {
         let label = UILabel()
         label.font = .preferredFont(forTextStyle: .subheadline)
         label.textColor = .accent
         
         return label
     }()
-    private let subTitleLabel = {
+    let subTitleLabel: UILabel = {
         let label = UILabel()
         label.font = .preferredFont(forTextStyle: .caption2)
         label.textColor = .subTitle
         
         return label
     }()
-    private let cellBackgroundView: UIView = {
+    let cellBackgroundView: UIView = {
         let backgroundView = UIView()
         backgroundView.backgroundColor = .mainCellBackground
         backgroundView.layer.cornerRadius = 10
@@ -36,47 +38,21 @@ final class MainTableViewCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         setupLayout()
+        selectionStyle = .none
+        backgroundColor = .clear
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        
-        titleLabel.hideSkeleton()
-        subTitleLabel.hideSkeleton()
-        isUserInteractionEnabled = true
-    }
-    
-    func configureSkeleton(with item: MainNotice) {
-        DispatchQueue.main.async {
-            self.titleLabel.text = item.notice.title
-            self.subTitleLabel.text = "[\(item.notice.department)]  \(item.notice.uploadDate)"
-            
-            self.titleLabel.isSkeletonable = true
-            self.subTitleLabel.isSkeletonable = true
-            
-            self.titleLabel.showAnimatedSkeleton()
-            self.subTitleLabel.showAnimatedSkeleton()
-            
-            self.isUserInteractionEnabled = false
-        }
-    }
-    
     func configure(with item: MainNotice) {
         titleLabel.text = item.notice.title
         subTitleLabel.text = "[\(item.notice.department)]  \(item.notice.uploadDate)"
-        
-        self.isUserInteractionEnabled = true
     }
     
-    private func setupLayout() {
+    func setupLayout() {
         contentView.addSubview(cellBackgroundView)
-        contentView.addSubview(titleLabel)
-        contentView.addSubview(subTitleLabel)
-        
         cellBackgroundView.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(10)
             make.leading.equalToSuperview().offset(16)
@@ -84,12 +60,14 @@ final class MainTableViewCell: UITableViewCell {
             make.bottom.equalToSuperview().offset(-10)
         }
         
+        contentView.addSubview(titleLabel)
         titleLabel.snp.makeConstraints { make in
             make.top.equalTo(cellBackgroundView.snp.top).offset(16)
             make.leading.equalTo(cellBackgroundView.snp.leading).offset(16)
             make.trailing.equalTo(cellBackgroundView.snp.trailing).offset(-16)
         }
         
+        contentView.addSubview(subTitleLabel)
         subTitleLabel.snp.makeConstraints { make in
             make.top.equalTo(titleLabel.snp.bottom).offset(10)
             make.leading.equalTo(cellBackgroundView.snp.leading).offset(16)
