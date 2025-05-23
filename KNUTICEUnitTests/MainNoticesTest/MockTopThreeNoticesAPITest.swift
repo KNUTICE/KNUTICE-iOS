fde7d5c70e32f06191364fc0b4c66a0a08e14def
jeungHunLee <ksjs1111@gmail.com>
2025-05-23 09:38:21 +0900

    TEST: 취업공지 테스트 추가

--- KNUTICEUnitTests/MainNoticesTest/MockTopThreeNoticesAPITest.swift
+++ KNUTICEUnitTests/MainNoticesTest/MockTopThreeNoticesAPITest.swift
@@ -11,7 +11,7 @@ final class MockTopThreeNoticesAPITests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         let configuration = URLSessionConfiguration.default
-        configuration.protocolClasses = [MockTopThreeNoticesURLProtocol.self]
+        configuration.protocolClasses = [MockURLProtocol.self]
         let session = Session(configuration: configuration)
         Container.shared.remoteDataSource.register {
             RemoteDataSourceImpl(session: session)
@@ -28,13 +28,18 @@ final class MockTopThreeNoticesAPITests: XCTestCase {
     
     func test_fetchTopThreeNotices_returnsMainNoticeResponseDTO() {
         let expectation = XCTestExpectation(description: "Wait for fetch top three notices")
+        MockURLProtocol.setUpMockData(.fetchTopThreeNoticesShouldSucceed)
+        
         dataSource.sendGetRequest(to: Bundle.main.mainNoticeURL, resultType: MainNoticeResponseDTO.self)
-            .sink {
-                print($0)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    expectation.fulfill()
+                case .failure(let error):
+                    XCTFail("\(error)")
+                }
             } receiveValue: { dto in
-                print(dto)
                 XCTAssertEqual(dto.result.resultCode, 200)
-                expectation.fulfill()
             }
             .store(in: &cancellables)
         
