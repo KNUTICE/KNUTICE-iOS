ff5e48626fb56a739a26ec3049b4aaff12fd5731
jeungHunLee <ksjs1111@gmail.com>
2025-07-22 13:19:33 +0900

    REFACTOR: SearchBar 오토레이아웃 재설정 로직 수정

--- KNUTICE/Presentation/SearchTable/View/SearchCollectionViewController+Layout.swift
+++ KNUTICE/Presentation/SearchTable/View/SearchCollectionViewController+Layout.swift
@@ -30,28 +30,25 @@ extension SearchCollectionViewController {
     }
     
     func updateSearchBarConstraints(isShowCancelButton: Bool) {
-        searchBar.snp.removeConstraints()
-        cancelButton.snp.removeConstraints()
-        
         if isShowCancelButton {
-            cancelButton.snp.makeConstraints { make in
+            cancelButton.snp.remakeConstraints { make in
                 make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
                 make.trailing.equalToSuperview()
                 make.centerY.equalTo(searchBar.snp.centerY)
             }
             
-            searchBar.snp.makeConstraints { make in
+            searchBar.snp.remakeConstraints { make in
                 make.leading.equalToSuperview()
                 make.trailing.equalTo(cancelButton.snp.leading).offset(10)
                 make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             }
         } else {
-            cancelButton.snp.makeConstraints { make in
+            cancelButton.snp.remakeConstraints { make in
                 make.leading.equalTo(view.snp.trailing)
                 make.centerY.equalTo(searchBar.snp.centerY)
             }
             
-            searchBar.snp.makeConstraints { make in
+            searchBar.snp.remakeConstraints { make in
                 make.leading.equalToSuperview()
                 make.trailing.equalToSuperview()
                 make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
