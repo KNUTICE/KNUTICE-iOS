b8588677d5dc730c5c14f5b82a8115e79e9e1e12
jeungHunLee <ksjs1111@gmail.com>
2025-07-22 11:27:46 +0900

    DESIGN: 북마크 구독 캡션 설명 수정

--- KNUTICE/Presentation/TopicSubscriptionList/View/TopicSubscriptionList.swift
+++ KNUTICE/Presentation/TopicSubscriptionList/View/TopicSubscriptionList.swift
@@ -17,39 +17,6 @@ struct TopicSubscriptionList: View {
     var body: some View {
         ZStack {
             List {
-//                Section {
-//                    Toggle(isOn: $viewModel.isEtiquetteTimeActivate) {
-//                        Text("에티켓 시간")
-//                    }
-//                    
-//                    if viewModel.isEtiquetteTimeActivate == true {
-//                        DatePicker(
-//                            "시작 시간",
-//                            selection: Binding(
-//                                get: {
-//                                    viewModel.etiquetteTimeStart ?? Date.tenPM
-//                                }, set: {
-//                                    viewModel.etiquetteTimeStart = $0
-//                                }
-//                            ),
-//                            displayedComponents: .hourAndMinute
-//                        )
-//                        
-//                        DatePicker(
-//                            "종료 시간",
-//                            selection: Binding(
-//                                get: {
-//                                    viewModel.etiquetteTimeEnd ?? Date.eightAM
-//                                }, set: {
-//                                    viewModel.etiquetteTimeEnd = $0
-//                                }
-//                            ),
-//                            displayedComponents: .hourAndMinute)
-//                    }
-//                } footer: {
-//                    Text("에티켓 시간 동안 알림을 받지 않습니다.")
-//                }
-                
                 Section {
                     Toggle(
                         isOn: Binding(
@@ -62,7 +29,7 @@ struct TopicSubscriptionList: View {
                             Text("일반소식")
                         })
                 } footer: {
-                    Text("일반소식은 학교의 주요 공지와 다양한 공지 사항이 포함됩니다.")
+                    Text("학교의 주요 공지와 관련된 다양한 소식을 알려드려요.")
                 }
                 
                 Section {
@@ -77,7 +44,7 @@ struct TopicSubscriptionList: View {
                             Text("학사공지")
                         })
                 } footer: {
-                    Text("학사공지는 수강, 성적, 졸업 등 학사 운영과 관련된 중요한 공지 사항이 포함됩니다.")
+                    Text("수강, 성적, 졸업 등 학사 운영과 관련된 다양한 소식을 알려드려요.")
                 }
                 
                 Section {
@@ -92,7 +59,7 @@ struct TopicSubscriptionList: View {
                             Text("장학안내")
                         })
                 } footer: {
-                    Text("장학안내는 다양한 장학금의 신청 자격, 절차, 일정 등 다양한 공지 사항이 포함됩니다.")
+                    Text("장학금의 신청 자격, 절차, 일정 등과 관련된 다양한 소식을 알려드려요.")
                 }
                 
                 Section {
@@ -107,7 +74,7 @@ struct TopicSubscriptionList: View {
                             Text("행사안내")
                         })
                 } footer: {
-                    Text("행사안내는 학교에서 진행되는 각종 교육, 문화, 진로 등 다양한 행사의 일정에 대한 공지 사항이 포함됩니다.")
+                    Text("학교에서 진행되는 각종 교육, 문화, 진로와 관련된 다양한 소식을 알려드려요.")
                 }
                 
                 Section {
@@ -123,7 +90,7 @@ struct TopicSubscriptionList: View {
                         }
                     )
                 } footer: {
-                    Text("취업안내는 채용 정보, 취업 지원 프로그램, 진로 상담 등 학생들의 진로 설계를 돕기 위한 공지 사항이 포합됩니다.")
+                    Text("채용 정보, 취업 지원 프로그램, 진로 상담 등 학생들의 진로 설계를 돕기 위한 소식을 알려드려요.")
                 }
             }
             .background(.primaryBackground)
