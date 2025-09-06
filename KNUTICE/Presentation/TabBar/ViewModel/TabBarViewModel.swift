eb4bd08b34d5b73362253cc1f5516c4642aa7558
jeungHunLee <ksjs1111@gmail.com>
2025-09-06 11:18:29 +0900

    REMOVE: 긴급 공지 기능 삭제

--- KNUTICE/Presentation/TabBar/ViewModel/TabBarViewModel.swift
+++ KNUTICE/Presentation/TabBar/ViewModel/TabBarViewModel.swift
@@ -12,28 +12,11 @@ import RxRelay
 import os
 
 final class TabBarViewModel {
-    let mainPopupContent: BehaviorRelay<MainPopupContent?> = .init(value: nil)
     let pushNotice: BehaviorRelay<Notice?> = .init(value: nil)
     
     @Injected(\.tabBarService) private var service: TabBarService
     private var cancellables: Set<AnyCancellable> = []
     private let logger: Logger = Logger()
-    
-    func fetchMainPopupContent() {
-        service.fetchMainPopupContent()
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] completion in
-                switch completion {
-                case .finished:
-                    self?.logger.debug("Successfully fetched a mainPopupContent")
-                case .failure(let error):
-                    self?.logger.error("TabBarViewModel.fetchMainPopupContent(): Failed to fetch a mainPopupContent: \(error)")
-                }
-            } receiveValue: { [weak self] in
-                self?.mainPopupContent.accept($0)
-            }
-            .store(in: &cancellables)
-    }
     
     func fetchPushNoticeIfExists() {
         service.fetchPushNotice()
