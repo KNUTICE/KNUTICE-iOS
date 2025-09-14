853b3208007bcd2c2152fbe772376a80bad6d2e3
jeungHunLee <ksjs1111@gmail.com>
2025-09-14 16:27:36 +0900

    REFACTOR: Tip API 마이그레이션

---
+++ KNUTICE/DTO/TipResponseDTO.swift
@@ -0,0 +1,28 @@
+//
+//  TipResponseDTO.swift
+//  KNUTICE
+//
+//  Created by 이정훈 on 7/1/25.
+//
+
+import Foundation
+import KNUTICECore
+
+// MARK: - TipResponseDTO
+struct TipResponseDTO: Decodable, Sendable {
+    let success: Bool
+    let code: Int
+    let message: String?
+    let data: [TipData]?
+}
+
+// MARK: - TipData
+struct TipData: Codable {
+    let tipID, title, url, deviceType: String
+    let createdAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case tipID = "tipId"
+        case title, url, deviceType, createdAt
+    }
+}
