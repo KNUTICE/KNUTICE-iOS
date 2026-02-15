2f19623aaab6b1fcacce7bff1ba6139e6ecc2aae
jeungHunLee <ksjs1111@gmail.com>
2026-02-15 21:10:51 +0900

    FEAT: 학식 조회 기능 추가

---
+++ Projects/KNMeal/Sources/Bundle+URL.swift
@@ -0,0 +1,27 @@
+//
+//  Bundle+URL.swift
+//  KNMeal
+//
+//  Created by 이정훈 on 2/15/26.
+//
+
+import Foundation
+
+extension Bundle {
+    var resource: NSDictionary? {
+        guard let file = self.path(forResource: "ServiceInfo", ofType: "plist"),
+              let resource = NSDictionary(contentsOfFile: file) else {
+            return nil
+        }
+        
+        return resource
+    }
+    
+    var baseURL: String {
+        guard let resource, let baseURL = resource["Base_URL"] as? String else {
+            return ""
+        }
+        
+        return baseURL
+    }
+}
