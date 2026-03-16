8e570946a334bf185c98b4fb15cb4455ee705c20
jeungHunLee <ksjs1111@gmail.com>
2026-03-16 16:48:28 +0900

    REFACTOR: KNMeal 모듈 Bundle 접근 로직 수정

--- Projects/KNMeal/Sources/Bundle+URL.swift
+++ Projects/KNMeal/Sources/Bundle+URL.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 extension Bundle {
+    static var knMeal: Bundle { Bundle.module }
+    
     var resource: NSDictionary? {
         guard let file = self.path(forResource: "ServiceInfo", ofType: "plist"),
               let resource = NSDictionary(contentsOfFile: file) else {
