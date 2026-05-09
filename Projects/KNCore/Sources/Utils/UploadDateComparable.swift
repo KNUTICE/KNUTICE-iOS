a708427122a863594ef0c0f2799fc24501aff31d
jeungHunLee <ksjs1111@gmail.com>
2026-05-09 11:15:05 +0900

    FIX: 업로드 날짜 확인 버그 수정

--- Projects/KNCore/Sources/Utils/UploadDateComparable.swift
+++ Projects/KNCore/Sources/Utils/UploadDateComparable.swift
@@ -15,8 +15,9 @@ extension UploadDateComparable {
     func isWithin24Hours(from dateString: String) -> Bool {
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd"
+        formatter.locale = Locale(identifier: "en_US_POSIX")    // 사용자 설정에 의해 포맷이 변경될 수 있어, en_US_POSIX로 포맷 고정
         let date = formatter.date(from: dateString) ?? Date.distantPast
         
-        return abs(date.timeIntervalSince1970) < 86400
+        return abs(Date().timeIntervalSince(date)) < 86400    // 24시간(86400)초 보다 이내인지 확인
     }
 }
