f4f8849357817031f934cca53791812f9efa9411
jeungHunLee <ksjs1111@gmail.com>
2026-06-18 08:36:29 +0900

    MOVE: KNSetting 모듈 디렉토리 이동

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -53,7 +53,7 @@ let project = Project(
                 .project(target: "KNDeepLink", path: "../KNDeepLink"),
                 .project(target: "KNTip", path: "../Features/KNTip"),
                 .project(target: "UIComponents", path: "../UIComponents"),
-                .project(target: "KNSetting", path: "../KNSetting"),
+                .project(target: "KNSetting", path: "../Features/KNSetting"),
                 .project(target: "KNReadingRoom", path: "../Features/KNReadingRoom"),
                 .project(target: "KNMeal", path: "../Features/KNMeal"),
                 .project(target: "KNDomain", path: "../KNDomain"),
