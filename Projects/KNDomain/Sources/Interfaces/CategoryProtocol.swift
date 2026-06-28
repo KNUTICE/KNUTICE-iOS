7f206513fdc37e55b05a8b35dd786d38250d9caa
jeungHunLee <ksjs1111@gmail.com>
2026-06-28 16:14:31 +0900

    CHORE: Xcode 주석 추가

--- Projects/KNDomain/Sources/Interfaces/CategoryProtocol.swift
+++ Projects/KNDomain/Sources/Interfaces/CategoryProtocol.swift
@@ -8,8 +8,10 @@
 import Foundation
 
 public protocol CategoryProtocol: Sendable {
+    
     /// A localized, human-readable name of the category for display in the user interface.
     var localizedDescription: String { get }
+    
     /// A legacy string identifier for the category.
     ///
     /// This property is deprecated and retained only for backward compatibility.
